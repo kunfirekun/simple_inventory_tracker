dbd76096286b17d3c45f90230db7b26fee0d2a0d
kunfirekun <okello_daniel@hotmail.com>
2022-06-30 21:35:07 +0300

    login file edited to recognize different roles logging in

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM consumers WHERE username = ? AND acc_status = '1' ";
+        $sql = "SELECT id, username, password FROM consumers WHERE username = ? AND role = 'admin' ";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -64,20 +64,60 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: index.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password";
                         }
                     }
                 } elseif (mysqli_stmt_num_rows($stmt) == 0){
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Confirm your account using the code sent to your email. <a href='confirm_email.php'>Click Here</a>";
+                    $sql = "SELECT id, username, password FROM consumers WHERE username = ? AND role = 'consumer' ";
+                       if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
                 }
+                 if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: userpanel.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password";
+                        }
+                    }
+                
+                }}}
+                
+                
+                
+                
+                
+                
+                
+                
 
                 
             } else{
-                echo "Something wrong has happened to our system.";
+                 $login_err = "Confirm your account. <a href='confirm_email.php'>Click Here</a>";
             }
 
             // Close statement
