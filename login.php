9bd0c10abfd645abce58fc67373c300e3209b25a
kunfirekun <okello_daniel@hotmail.com>
2022-06-30 23:10:01 +0300

    login page redirections to home admin and home public pages upon login

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: login.php");
     exit;
 }
  
@@ -64,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: index.php");
+                            header("location: home-admin.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password";
@@ -98,7 +98,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: userpanel.php");
+                            header("location: home-public.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password";
