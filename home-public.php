dfde638f934f7adf7206170e85e25734fe214d5c
kunfirekun <okello_daniel@hotmail.com>
2022-07-01 10:05:34 +0300

    edits to admin and public account pages

--- home-public.php
+++ home-public.php
@@ -1,43 +1,29 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Home | </title>
-    <meta name="robots" content="noindex" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.5" />
-    <link rel="stylesheet" href="admin.css" />
-  </head>
-  <body>
-    <!-- (A) SIDEBAR -->
-    <div id="pgside">
-      <!-- (A1) BRANDING OR USER -->
-      <!-- LINK TO DASHBOARD OR LOGOUT -->
-      <div id="pguser">
-        <img src="potato.png"/>
-        <i class="txt">MY ADMIN</i>
-      </div>
-
-      <!-- (A2) MENU ITEMS -->
-      <a href="#" class="current">
-        <i class="ico">&#9733;</i>
-        <i class="txt">Section A</i>
-      </a>
-      <a href="#">
-        <i class="ico">&#9728;</i>
-        <i class="txt">Section B</i>
-      </a>
-      <a href="#">
-        <i class="ico">&#9737;</i>
-        <i class="txt">Section C</i>
-      </a>
-      <a href="logout.php">
-        <i class="ico">&#9737;</i>
-        <i class="txt">Log Out</i>
-      </a>
-    </div>
-
-    <!-- (B) MAIN -->
-    <main id="pgmain">
-      Main
-    </main>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+</body>
 </html>
