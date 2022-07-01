cdd1a6ccc49db03848104d9f1a187bb2bbc0fa3e
kunfirekun <okello_daniel@hotmail.com>
2022-07-01 18:03:35 +0300

    checkslaes php and button on home page

---
+++ checksales.php
@@ -0,0 +1,1 @@
+checksales.php
--- home-admin.php
+++ home-admin.php
@@ -22,6 +22,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <h1 class="my-5">Hi, Administrator <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
+    <a href="checksales.php" class="btn btn-warning">Check Sales</a>
+    </p>
+    <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
