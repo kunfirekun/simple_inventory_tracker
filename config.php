6946a41f8a7a93260e13859e0c79440202001451
Dan Okello <kunfirekun@gmail.com>
2022-07-02 10:23:14 +0300

    changes to the file
    changes to the public file

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'eazzzydi');
-define('DB_PASSWORD', 'Guap2000$');
-define('DB_NAME', 'eazzzydi_playground');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
